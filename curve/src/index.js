8f957911e3faa51db70486a1a459d4f2c95234c5
EloK6 <ecassignol@gmail.com>
2019-11-11 11:59:31 +0100

    add curve

---
+++ curve/src/index.js
@@ -0,0 +1,102 @@
+import {
+  select,
+  csv,
+  scaleTime,
+  extent,
+  axisLeft,
+  axisBottom,
+  scaleLinear,
+  line,
+  curveBasis
+} from "d3";
+
+const svg = select("svg");
+
+const width = +svg.attr("width");
+const height = +svg.attr("height");
+
+const render = data => {
+  const title = "A week in San Francisco";
+
+  const xValue = d => d.timestamp;
+  const xAxisLabel = "Time";
+
+  const yValue = d => d.temperature;
+  const yAxisLabel = "Temperature";
+
+  const margin = { top: 60, right: 40, bottom: 88, left: 150 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = scaleTime()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([innerHeight, 0])
+    .nice();
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xAxis = axisBottom(xScale)
+    .tickSize(-innerHeight)
+    .tickPadding(15);
+
+  const yAxis = axisLeft(yScale)
+    .tickSize(-innerWidth)
+    .tickPadding(10);
+
+  const yAxisG = g.append("g").call(yAxis);
+  yAxisG.selectAll(".domain").remove();
+
+  yAxisG
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", -93)
+    .attr("x", -innerHeight / 2)
+    .attr("fill", "black")
+    .attr("transform", `rotate(-90)`)
+    .attr("text-anchor", "middle")
+    .text(yAxisLabel);
+
+  const xAxisG = g
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0,${innerHeight})`);
+
+  xAxisG.select(".domain").remove();
+
+  xAxisG
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", 75)
+    .attr("x", innerWidth / 2)
+    .attr("fill", "black")
+    .text(xAxisLabel);
+
+  const lineGenerator = line()
+    .x(d => xScale(xValue(d)))
+    .y(d => yScale(yValue(d)))
+    .curve(curveBasis);
+
+  g.append("path")
+    .attr("class", "line-path")
+    .attr("d", lineGenerator(data));
+
+  g.append("text")
+    .attr("class", "title")
+    .attr("y", -10)
+    .text(title);
+};
+
+csv("data.csv").then(data => {
+  data.forEach(d => {
+    d.temperature = +d.temperature;
+    d.timestamp = new Date(d.timestamp);
+  });
+  render(data);
+});
